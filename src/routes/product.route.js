aa37d6a519c30b3e8cddad97ace6ab8e14cd2a5c
boyka9x <huanzoyza@gmail.com>
2022-08-08 22:15:50 +0700

    route

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const ProductController = require('../controller/product.controller');
+const { verifyToken, verifyPermission } = require('../middleware/authenticate');
 
 router.get('/', ProductController.getList);
 router.get('/:id/edit', ProductController.getById);
 router.get('/deleted', ProductController.getListDeleted);
 router.get('/:slug', ProductController.getBySlug);
-router.post('/', ProductController.create);
-router.put('/:id', ProductController.update);
-router.patch('/:id/restore', ProductController.restore);
-router.delete('/:id', ProductController.delete);
+router.post('/', verifyToken, verifyPermission(['admin']), ProductController.create);
+router.put('/:id', verifyToken, verifyPermission(['admin']), ProductController.update);
+router.patch('/:id/restore', verifyToken, verifyPermission(['admin']), ProductController.restore);
+router.delete('/:id', verifyToken, verifyPermission(['admin']), ProductController.delete);
 
 module.exports = router;
